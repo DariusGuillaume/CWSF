ddea26e8d00b0d93710f47059538446f09f38305
DariusGuillaume <dariusguillaume2@gmail.com>
2024-07-02 22:59:14 -0400

    added project base

---
+++ microservicesframwork/src/main/java/com/darius/microservicesframwork/MicroservicesframworkApplication.java
@@ -0,0 +1,13 @@
+package com.darius.microservicesframwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesframworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesframworkApplication.class, args);
+	}
+
+}
---
+++ microservicesframwork/src/test/java/com/darius/microservicesframwork/MicroservicesframworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darius.microservicesframwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesframworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
